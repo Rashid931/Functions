5fb27a3dbc1c5f1b2f2d3810267bf8a7061dde71
Rashid931 <rashid@cae.nust.edu.pk>
2024-07-19 08:36:10 +0500

    Add help.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,14 @@
 def factorial (n):
+    """
+    n: Any integer
+    return factorial
+    """
     if (n==0 or n==1):
         return 1
     else:
         return n*factorial (n-1)
 
-
 n = 6
 print (f"The factorial of {n} is {factorial(n)}")
+
+help (factorial)
